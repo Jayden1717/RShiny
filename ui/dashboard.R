84a7d4819308e182c7d7582f9b6a982f3d9b5857
Jayden1717 <47407165+Jayden1717@users.noreply.github.com>
2022-08-10 22:43:45 +1000

    commit test
    commit test

---
+++ ui/dashboard.R
@@ -0,0 +1,26 @@
+fluidPage(
+  tabsetPanel(id = 'home_tabset',
+              tabPanel('Home',
+                       tabsetPanel(
+                         tabPanel(value = 'Subtab 1',
+                                  title = 'SUB TAB 1',
+                                  uiOutput(outputId = "subtab1_ui")
+                         ),
+                         tabPanel(value = 'Subtab 2',
+                                  title = 'SUB TAB 2'
+                                  # uiOutput('subtab2_ui')
+                         )
+                       )
+              ),
+              tabPanel('Home Page 2',
+                       tabsetPanel(
+                         tabPanel('Subtab 3',
+                                  # uiOutput('subtab3_ui')
+                         ),
+                         tabPanel('Subtab 4',
+                                  # uiOutput('subtab4_ui')
+                         )
+                       )
+              )
+  )
+)
